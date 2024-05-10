e49d8a3234d0b847877037988516bf474a26e346
Domagoj Krajnovic <equilento@gmail.com>
2024-05-10 21:26:53 +0200

    Update __loading__.js

--- __loading__.js
+++ __loading__.js
@@ -44,7 +44,7 @@ pc.script.createLoadingScreen(function (app) {
     var createCss = function () {
         var css = [
             'body {',
-            '    background-color: #F6F2E8;',
+            '    background-color: #614247;',
             '}',
 
             '#application-splash-wrapper {',
@@ -53,7 +53,7 @@ pc.script.createLoadingScreen(function (app) {
             '    left: 0;',
             '    height: 100%;',
             '    width: 100%;',
-            '    background-color: #F6F2E8;',
+            '    background-color: #614247;',
             '}',
 
             '#application-splash {',
